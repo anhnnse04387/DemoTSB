071e8d68d55f23a156a8d198f9bb9925be054974
DESKTOP-UNQK7SS\Brdl666 <anhnnse04387@fpt.edu.vn>
2017-12-13 22:14:57 +0700

    update

--- Thai Son Bac Demo/asset/js/main.js
+++ Thai Son Bac Demo/asset/js/main.js
@@ -1,3 +1,237 @@
+function btnAddClicked() {
+    swal("Successfully", "You created an order with id - O1349", "success");
+}
+
+function configSp1() {
+    var ck1 = 0;
+    if (parseInt(document.getElementById("ck1").value) > 0) {
+        ck1 = parseInt(document.getElementById("ck1").value);
+    }
+    if (document.getElementById("cai_1").value !== '') {
+        if (document.getElementById("cai_1").value !== '0') {
+            var cai = parseInt(document.getElementById("cai_1").value);
+            if (ck1 !== 0) {
+                document.getElementById("tien_1").value = cai * parseInt($("#dongia_1").val()) * (100 - ck1) / 100;
+            } else {
+                document.getElementById("tien_1").value = cai * parseInt($("#dongia_1").val());
+            }
+            document.getElementById("thung_1").value = parseInt(cai / 20);
+        } else {
+            document.getElementById("thung_1").value = 0;
+            document.getElementById("tien_1").value = 0;
+            document.getElementById("tong_thung").value = 0;
+            document.getElementById("tong_cai").value = 0;
+            document.getElementById("tien_chua_ck").value = 0;
+            document.getElementById("tien_da_ck").value = 0;
+        }
+        var tien_chua_ck = parseInt($("#tien_1").val()) +
+                ($("#tien_2").val() === '' ? 0 : parseInt($("#tien_2").val())) +
+                ($("#tien_3").val() === '' ? 0 : parseInt($("#tien_3").val()));
+        var tong_thung = ($("#thung_1").val() === '' ? 0 : parseInt($("#thung_1").val())) +
+                ($("#thung_2").val() === '' ? 0 : parseInt($("#thung_2").val())) +
+                ($("#thung_3").val() === '' ? 0 : parseInt($("#thung_3").val()));
+        var tong_cai = (document.getElementById("cai_1").value === '' ? 0 : parseInt(document.getElementById("cai_1").value)) +
+                (document.getElementById("cai_2").value === '' ? 0 : parseInt(document.getElementById("cai_2").value)) +
+                (document.getElementById("cai_3").value === '' ? 0 : parseInt(document.getElementById("cai_3").value));
+        var ck = 0;
+        document.getElementById("tong_thung").value = tong_thung;
+        document.getElementById("tong_cai").value = tong_cai;
+        if (parseFloat(document.getElementById("ck").value) > 0) {
+            ck = parseFloat(document.getElementById("ck").value);
+        }
+        document.getElementById("tien_chua_ck").value = tien_chua_ck;
+        var tien_ck = tien_chua_ck * ck / 100;
+        document.getElementById("tien_ck").value = tien_ck;
+        document.getElementById("tien_da_ck").value = tien_chua_ck - tien_ck;
+    } else {
+        document.getElementById("thung_1").value = 0;
+        document.getElementById("tien_1").value = 0;
+        document.getElementById("tong_thung").value = 0;
+        document.getElementById("tong_cai").value = 0;
+        document.getElementById("tien_chua_ck").value = 0;
+        document.getElementById("tien_da_ck").value = 0;
+    }
+}
+
+function configCk1() {
+    var cai = 0;
+    var ck1 = 0;
+    if (parseInt(document.getElementById("ck1").value) > 0) {
+        ck1 = parseInt(document.getElementById("ck1").value);
+    }
+    if (parseInt(document.getElementById("cai_1").value) > 0) {
+        cai = parseInt(document.getElementById("cai_1").value);
+    }
+    if (cai > 0) {
+        if (ck1 !== 0) {
+            document.getElementById("tien_1").value = cai * parseInt($("#dongia_1").val()) * (100 - ck1) / 100;
+        }
+    }
+}
+
+function configCk2() {
+    var cai = 0;
+    var ck2 = 0;
+    if (parseInt(document.getElementById("ck2").value) > 0) {
+        ck2 = parseInt(document.getElementById("ck2").value);
+    }
+    if (parseInt(document.getElementById("cai_2").value) > 0) {
+        cai = parseInt(document.getElementById("cai_2").value);
+    }
+    if (cai > 0) {
+        if (ck2 !== 0) {
+            document.getElementById("tien_2").value = cai * parseInt($("#dongia_2").val()) * (100 - ck2) / 100;
+        }
+    }
+}
+
+function configCk3() {
+    var cai = 0;
+    var ck3 = 0;
+    if (parseInt(document.getElementById("ck3").value) > 0) {
+        ck3 = parseInt(document.getElementById("ck3").value);
+    }
+    if (parseInt(document.getElementById("cai_3").value) > 0) {
+        cai = parseInt(document.getElementById("cai_3").value);
+    }
+    if (cai > 0) {
+        if (ck3 !== 0) {
+            document.getElementById("tien_3").value = cai * parseInt($("#dongia_3").val()) * (100 - ck3) / 100;
+        }
+    }
+}
+
+function configCk() {
+    var ck = 0;
+    var tien_chua_ck = 0;
+    if (parseInt(document.getElementById("tien_chua_ck").value) > 0) {
+        tien_chua_ck = parseInt(document.getElementById("tien_chua_ck").value);
+    }
+    if (parseFloat(document.getElementById("ck").value) > 0) {
+        ck = parseFloat(document.getElementById("ck").value);
+    }
+    var tien_ck = tien_chua_ck * ck / 100;
+    document.getElementById("tien_ck").value = tien_ck;
+    document.getElementById("tien_da_ck").value = tien_chua_ck - tien_ck;
+}
+
+function configSp2() {
+    var ck2 = 0;
+    if (parseInt(document.getElementById("ck1").value) > 0) {
+        ck2 = parseInt(document.getElementById("ck1").value);
+    }
+    if (document.getElementById("cai_2").value !== '') {
+        if (document.getElementById("cai_2").value !== '0') {
+            var cai = parseInt(document.getElementById("cai_2").value);
+            if (ck2 !== 0) {
+                document.getElementById("tien_2").value = cai * parseInt($("#dongia_2").val()) * (100 - ck2) / 100;
+            } else {
+                document.getElementById("tien_2").value = cai * parseInt($("#dongia_2").val());
+            }
+            document.getElementById("thung_2").value = parseInt(cai / 20);
+        } else {
+            document.getElementById("thung_2").value = 0;
+            document.getElementById("tien_2").value = 0;
+            document.getElementById("tong_thung").value = 0;
+            document.getElementById("tong_cai").value = 0;
+            document.getElementById("tien_chua_ck").value = 0;
+            document.getElementById("tien_da_ck").value = 0;
+        }
+        var tien_chua_ck = parseInt($("#tien_1").val()) +
+                ($("#tien_2").val() === '' ? 0 : parseInt($("#tien_2").val())) +
+                ($("#tien_3").val() === '' ? 0 : parseInt($("#tien_3").val()));
+        var tong_thung = ($("#thung_1").val() === '' ? 0 : parseInt($("#thung_1").val())) +
+                ($("#thung_2").val() === '' ? 0 : parseInt($("#thung_2").val())) +
+                ($("#thung_3").val() === '' ? 0 : parseInt($("#thung_3").val()));
+        var tong_cai = (document.getElementById("cai_1").value === '' ? 0 : parseInt(document.getElementById("cai_1").value)) +
+                (document.getElementById("cai_2").value === '' ? 0 : parseInt(document.getElementById("cai_2").value)) +
+                (document.getElementById("cai_3").value === '' ? 0 : parseInt(document.getElementById("cai_3").value));
+        var ck = 0;
+        document.getElementById("tong_thung").value = tong_thung;
+        document.getElementById("tong_cai").value = tong_cai;
+        if (parseFloat(document.getElementById("ck").value) > 0) {
+            ck = parseFloat(document.getElementById("ck").value);
+        }
+        document.getElementById("tien_chua_ck").value = tien_chua_ck;
+        var tien_ck = tien_chua_ck * ck / 100;
+        document.getElementById("tien_ck").value = tien_ck;
+        document.getElementById("tien_da_ck").value = tien_chua_ck - tien_ck;
+    } else {
+        document.getElementById("thung_2").value = 0;
+        document.getElementById("tien_2").value = 0;
+        document.getElementById("tong_thung").value = 0;
+        document.getElementById("tong_cai").value = 0;
+        document.getElementById("tien_chua_ck").value = 0;
+        document.getElementById("tien_da_ck").value = 0;
+    }
+}
+
+function configSp3() {
+    var ck3 = 0;
+    if (parseInt(document.getElementById("ck1").value) > 0) {
+        ck3 = parseInt(document.getElementById("ck1").value);
+    }
+    if (document.getElementById("cai_3").value !== '') {
+        if (document.getElementById("cai_3").value !== '0') {
+            var cai = parseInt(document.getElementById("cai_3").value);
+            if (ck3 !== 0) {
+                document.getElementById("tien_3").value = cai * parseInt($("#dongia_3").val()) * (100 - ck3) / 100;
+            } else {
+                document.getElementById("tien_3").value = cai * parseInt($("#dongia_3").val());
+            }
+            document.getElementById("thung_3").value = parseInt(cai / 20);
+        } else {
+            document.getElementById("thung_3").value = 0;
+            document.getElementById("tien_3").value = 0;
+            document.getElementById("tong_thung").value = 0;
+            document.getElementById("tong_cai").value = 0;
+            document.getElementById("tien_chua_ck").value = 0;
+            document.getElementById("tien_da_ck").value = 0;
+        }
+        var tien_chua_ck = parseInt($("#tien_1").val()) +
+                ($("#tien_2").val() === '' ? 0 : parseInt($("#tien_2").val())) +
+                ($("#tien_3").val() === '' ? 0 : parseInt($("#tien_3").val()));
+        var tong_thung = ($("#thung_1").val() === '' ? 0 : parseInt($("#thung_1").val())) +
+                ($("#thung_2").val() === '' ? 0 : parseInt($("#thung_2").val())) +
+                ($("#thung_3").val() === '' ? 0 : parseInt($("#thung_3").val()));
+        var tong_cai = (document.getElementById("cai_1").value === '' ? 0 : parseInt(document.getElementById("cai_1").value)) +
+                (document.getElementById("cai_2").value === '' ? 0 : parseInt(document.getElementById("cai_2").value)) +
+                (document.getElementById("cai_3").value === '' ? 0 : parseInt(document.getElementById("cai_3").value));
+        var ck = 0;
+        document.getElementById("tong_thung").value = tong_thung;
+        document.getElementById("tong_cai").value = tong_cai;
+        if (parseFloat(document.getElementById("ck").value) > 0) {
+            ck = parseFloat(document.getElementById("ck").value);
+        }
+        document.getElementById("tien_chua_ck").value = tien_chua_ck;
+        var tien_ck = tien_chua_ck * ck / 100;
+        document.getElementById("tien_ck").value = tien_ck;
+        document.getElementById("tien_da_ck").value = tien_chua_ck - tien_ck;
+    } else {
+        document.getElementById("thung_3").value = 0;
+        document.getElementById("tien_3").value = 0;
+        document.getElementById("tong_thung").value = 0;
+        document.getElementById("tong_cai").value = 0;
+        document.getElementById("tien_chua_ck").value = 0;
+        document.getElementById("tien_da_ck").value = 0;
+    }
+}
+
+function lookup() {
+    if ($("#donVi").val() === 'FPT') {
+        document.getElementById("AddressDeliver").value = "Hoa Lac";
+        document.getElementById("AddressContact").value = "(84-24) 7300 2222";
+        document.getElementById("TaxCode").value = "1";
+    } else if ($("#donVi").val() === 'Viettel') {
+        document.getElementById("AddressDeliver").value = "Hoa Lac";
+        document.getElementById("AddressContact").value = "024.62 818 818";
+        document.getElementById("TaxCode").value = "2";
+    } else if ($("#donVi").val() === 'CMC') {
+        document.getElementById("AddressDeliver").value = "Duy Tan";
+        document.getElementById("AddressContact").value = "1900 2101";
+        document.getElementById("TaxCode").value = "3";
+    }
+}
 (function (jQuery) {
 
     // Variable
