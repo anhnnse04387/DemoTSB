698ff009ca28fede4f47ac9c515535beb014f9e0
anhntlse04122 <34432092+anhntlse04122@users.noreply.github.com>
2017-12-29 10:09:00 +0700

    update

--- AdminLTS/AdminLTE-2.4.2/ThaiSonBac/dist/js/timeline.js
+++ AdminLTS/AdminLTE-2.4.2/ThaiSonBac/dist/js/timeline.js
@@ -1,18 +1,33 @@
 // DOM element where the Timeline will be attached
-var container = document.getElementById('timeline');
+var timeline = document.getElementById('timeline');
 
-// Create a DataSet (allows two way data-binding)
+var groups = new vis.DataSet([
+  { id: 0, content: 'Order 1' },
+  { id: 1, content: 'Order 2' },
+  { id: 2, content: 'Order 3' },
+  { id: 3, content: 'Order 4' },
+]);
+
+// create a dataset with items
 var items = new vis.DataSet([
-  {content: 'item 1 (100)', start: '2018-01-01'},
-  {content: 'item 2 (200)', start: '2018-01-02'},
-  {content: 'item 3 (200)', className: 'red', start: '2018-01-02T15:00:00'},
-  {content: 'item 4 (200)', start: '2018-01-04'},
-  {content: 'item 5 (300)', className: 'red', start: '2018-01-05'}
+  {id: 0, group: 0, content: '100', start: new Date(2018, 0, 03), end: new Date(2018, 0, 04)},
+  {id: 1, group: 1, content: '200', start: new Date(2018, 0, 04), end: new Date(2018, 0, 05)},
+  {id: 2, group: 2, content: '300', start: new Date(2018, 0, 05), end: new Date(2018, 0, 06)},
+  {id: 3, group: 3, content: '500', start: new Date(2018, 0, 07), end: new Date(2018, 0, 08)}
 ]);
 
 // Configuration for the Timeline
 var options = {
+  stack: true,
+  horizontalScroll: true,
+  maxHeight: 400,
+  start: new Date(2018, 0, 01),
+  end: new Date(2018, 0, 08),
+  margin: {
+    item: 10, // minimal margin between items
+    axis: 5   // minimal margin between items and the axis
+  }
 };
 
 // Create a Timeline
-var timeline = new vis.Timeline(container, items, options);
+var timeline = new vis.Timeline(timeline, items, groups, options);
