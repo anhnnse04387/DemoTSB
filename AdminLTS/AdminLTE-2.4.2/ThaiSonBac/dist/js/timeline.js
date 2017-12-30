1222664535f6db0168665681f998e7de34c4be2a
Hoa Duong <hoadx.fu@gmail.com>
2017-12-30 16:12:39 +0700

    combine timeline with adminlte

--- AdminLTS/AdminLTE-2.4.2/ThaiSonBac/dist/js/timeline.js
+++ AdminLTS/AdminLTE-2.4.2/ThaiSonBac/dist/js/timeline.js
@@ -1,18 +1,37 @@
 // DOM element where the Timeline will be attached
-var container = document.getElementById('timeline');
+var timeline = document.getElementById('timeline');
 
-// Create a DataSet (allows two way data-binding)
+var groups = new vis.DataSet([
+  { id: 0, content: 'Order 1' },
+  { id: 1, content: 'Order 2' },
+  { id: 2, content: 'Order 3' },
+  { id: 3, content: 'Order 4' },
+]);
+
+// create a dataset with items
 var items = new vis.DataSet([
-  {content: 'item 1 (100)', start: '2018-01-01'},
-  {content: 'item 2 (200)', start: '2018-01-02'},
-  {content: 'item 3 (200)', className: 'red', start: '2018-01-02T15:00:00'},
-  {content: 'item 4 (200)', start: '2018-01-04'},
-  {content: 'item 5 (300)', className: 'red', start: '2018-01-05'}
+  {id: 0, group: 0, content: 'Tạo đơn', start: new Date(2018, 0, 01)},
+  {id: 1, group: 0, content: 'Giao hàng', start: new Date(2018, 0, 03)},
+  {id: 2, group: 1, content: 'Tạo đơn', start: new Date(2018, 0, 02)},
+  {id: 3, group: 1, content: 'Giao hàng', start: new Date(2018, 0, 03)},
+  {id: 4, group: 2, content: 'Tạo đơn', start: new Date(2018, 0, 01)},
+  {id: 5, group: 2, content: 'Giao hàng', start: new Date(2018, 0, 04)},
+  {id: 6, group: 3, content: 'Tạo đơn', start: new Date(2018, 0, 03)},
+  {id: 7, group: 3, content: 'Giao hàng', start: new Date(2018, 0, 07)},
+  {id: 8, group: 4, content: 'Tạo đơn', start: new Date(2018, 0, 03)},
+  {id: 9, group: 4, content: 'Giao hàng', start: new Date(2018, 0, 04)}
 ]);
 
 // Configuration for the Timeline
 var options = {
+  stack: true,
+  horizontalScroll: true,
+  maxHeight: 400,
+  margin: {
+    item: 10, // minimal margin between items
+    axis: 5   // minimal margin between items and the axis
+  }
 };
 
 // Create a Timeline
-var timeline = new vis.Timeline(container, items, options);
+var timeline = new vis.Timeline(timeline, items, groups, options);
