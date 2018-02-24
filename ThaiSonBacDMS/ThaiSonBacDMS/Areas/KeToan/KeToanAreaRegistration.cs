817357f07b9b2f2660f9ee49b63e47f0dc0cc72f
DESKTOP-UNQK7SS\Brdl666 <anhnnse04387@fpt.edu.vn>
2018-02-25 02:18:42 +0700

    update

---
+++ ThaiSonBacDMS/ThaiSonBacDMS/Areas/KeToan/KeToanAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace ThaiSonBacDMS.Areas.KeToan
+{
+    public class KeToanAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "KeToan";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "KeToan_default",
+                "KeToan/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ ThaiSonBacDMS/ThaiSonBacDMS/Areas/PhanPhoi/PhanPhoiAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace ThaiSonBacDMS.Areas.PhanPhoi
+{
+    public class PhanPhoiAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "PhanPhoi";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "PhanPhoi_default",
+                "PhanPhoi/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
