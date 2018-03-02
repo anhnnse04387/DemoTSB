0544616721cd01f25ae479ec9ff25377bcc04f32
DESKTOP-UNQK7SS\Brdl666 <anhnnse04387@fpt.edu.vn>
2018-03-03 02:25:09 +0700

    update

---
+++ ThaiSonBacDMS/ThaiSonBacDMS/Areas/PhanPhoi/Controllers/LapPhieuController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ThaiSonBacDMS.Areas.PhanPhoi.Controllers
+{
+    public class LapPhieuController : Controller
+    {
+        // GET: PhanPhoi/LapPhieu
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
