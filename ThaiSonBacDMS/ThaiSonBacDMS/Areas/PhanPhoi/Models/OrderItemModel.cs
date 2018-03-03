6975260559f0e567ec9e9f3e913d70a0caca623b
DESKTOP-UNQK7SS\Brdl666 <anhnnse04387@fpt.edu.vn>
2018-03-04 00:28:11 +0700

    update

---
+++ ThaiSonBacDMS/ThaiSonBacDMS/Areas/PhanPhoi/Models/OrderItemModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ThaiSonBacDMS.Areas.PhanPhoi.Models
+{
+    public class OrderItemModel
+    {
+
+        public long productId { get; set; }
+        public int quantity { get; set; }
+        public int box { get; set; }
+        public int discount { get; set; }
+        public long price { get; set; }
+
+    }
+}
--- ThaiSonBacDMS/ThaiSonBacDMS/Areas/PhanPhoi/Models/OrderPartModel.cs
+++ ThaiSonBacDMS/ThaiSonBacDMS/Areas/PhanPhoi/Models/OrderPartModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,15 @@ namespace ThaiSonBacDMS.Areas.PhanPhoi.Models
 {
     public class OrderPartModel
     {
+
+        public int partId { get; set; }
+
+        public String orderPartId { get; set; }
+
+        [Required(ErrorMessage = "Xin hãy nhập ngày xuất hàng")]
+        public DateTime deliveryDay { get; set; }
+
+        public List<OrderItemModel> lstItem { get; set; }
+
     }
 }
