c5419f03b205f5fb046c17330fe5e9eba97e5386
DESKTOP-UNQK7SS\Brdl666 <anhnnse04387@fpt.edu.vn>
2018-03-06 01:13:35 +0700

    update

--- ThaiSonBacDMS/ThaiSonBacDMS/Areas/PhanPhoi/Controllers/LapPhieuController.cs
+++ ThaiSonBacDMS/ThaiSonBacDMS/Areas/PhanPhoi/Controllers/LapPhieuController.cs
@@ -13,22 +13,10 @@ namespace ThaiSonBacDMS.Areas.PhanPhoi.Controllers
         // GET: PhanPhoi/LapPhieu
         public ActionResult Index()
         {
-<<<<<<< HEAD
-
-            //setViewBag();
-            return View();
-=======
             var dao = new CustomerDAO();
             var model = new OrderTotalModel();
             model.lstCustomer = new SelectList(dao.getCustomer(), "Customer_ID", "Customer_name");
             return View(model);
->>>>>>> c5010b3600a2f6426226a542cb2e59792e0cec51
-        }
-
-        public void setViewBag(String selectedId = null)
-        {
-            var dao = new CustomerDAO();
-            ViewBag.customerId = new SelectList(dao.getCustomer(), "Customer_ID", "Customer_name", selectedId);
         }
     }
 }
