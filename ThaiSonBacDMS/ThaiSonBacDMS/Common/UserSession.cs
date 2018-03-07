9a0d847bc5587634b174b181de78fb4cffcedf9a
CuongNguyen04273 <34432069+CuongNguyen04273@users.noreply.github.com>
2018-03-07 15:45:08 +0700

    commit

---
+++ ThaiSonBacDMS/ThaiSonBacDMS/Common/UserSession.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ThaiSonBacDMS.Common
+{
+    [Serializable]
+    public class UserSession
+    {
+        public string accountID;
+        public string account_name;
+
+    }
+}
