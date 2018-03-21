582dfa432e3f4eb2f4fd2482865b734e70e8a759
CuongNguyen04273 <34432069+CuongNguyen04273@users.noreply.github.com>
2018-03-21 16:15:13 +0700

    commit

--- ThaiSonBacDMS/ThaiSonBacDMS/Areas/HangHoa/Controllers/HomeController.cs
+++ ThaiSonBacDMS/ThaiSonBacDMS/Areas/HangHoa/Controllers/HomeController.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Models.DAO;
+using Models.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ThaiSonBacDMS.Common;
 using ThaiSonBacDMS.Controllers;
 
 namespace ThaiSonBacDMS.Areas.HangHoa.Controllers
@@ -13,6 +16,51 @@ namespace ThaiSonBacDMS.Areas.HangHoa.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+        [ChildActionOnly]
+        public PartialViewResult NotificationHeader()
+        {
+            var notiDAO = new NotificationDAO();
+            var session = (UserSession)Session[CommonConstants.USER_SESSION];
+            List<Notification> listNoti = new List<Notification>();
+            listNoti = notiDAO.getByUserID(session.user_id);
+            return PartialView(listNoti);
+        }
+
+        [ChildActionOnly]
+        public PartialViewResult NoteEdit()
+        {
+            var noteDAO = new NoteDAO();
+            var session = (UserSession)Session[CommonConstants.USER_SESSION];
+            var content = noteDAO.getNotebyAccount(session.accountID);
+
+            return PartialView(content);
+        }
+
+        [HttpPost]
+        public PartialViewResult NoteEdit(Note note)
+        {
+            var noteDAO = new NoteDAO();
+            noteDAO.editNotebyAccount(note.Account_ID, note.Contents);
+            return PartialView();
+        }
+
+        [ChildActionOnly]
+        public PartialViewResult NoteHeader()
+        {
+            var noteDAO = new NoteDAO();
+            var session = (UserSession)Session[CommonConstants.USER_SESSION];
+            var content = noteDAO.getNotebyAccount(session.accountID).Contents;
+            string[] lines = new string[] { };
+            if (!string.IsNullOrEmpty(content.Trim()))
+            {
+                lines = content.Split(Environment.NewLine.ToCharArray());
+            }
+            else
+            {
+                lines = new string[] { "Hãy điền ghi chú vào đây" };
+            }
+            return PartialView(lines);
         }
     }
 }
