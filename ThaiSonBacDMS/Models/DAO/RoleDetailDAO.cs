d09ce1c85101a13b294552546de004baedb2297d
Xuan Thuong <thuongtxse03568@fpt.edu.vn>
2018-04-20 09:32:19 +0700

    commit code

---
+++ ThaiSonBacDMS/Models/DAO/RoleDetailDAO.cs
@@ -0,0 +1,23 @@
+﻿using Models.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.DAO
+{
+    public class RoleDetailDAO
+    {
+        public ThaiSonBacDMSDbContext db = null;
+
+        public RoleDetailDAO()
+        {
+            db = new ThaiSonBacDMSDbContext();
+        }
+        public List<Role_detail> lstAllRole()
+        {
+            return db.Role_detail.ToList();
+        }
+    }
+}
--- ThaiSonBacDMS/Models/DAO/UserDAO.cs
+++ ThaiSonBacDMS/Models/DAO/UserDAO.cs
@@ -49,37 +49,109 @@ namespace Models.DAO
             return context.Offices.SingleOrDefault(x => x.Office_ID == officeID).Office_name;
         }
         //thuongtx
-        public byte getRoleId()
+
+
+        public int getRoleIdByCurrentAcc(string userId)
+        {
+            return Convert.ToInt32(context.Users.SingleOrDefault(x => x.User_ID.Equals(userId)).Role_ID);
+        }
+        public int getRoleQuanTri()
         {
             return context.Role_detail.SingleOrDefault(x => x.Role_name.ToLower().Equals("quản trị")).Role_ID;
         }
-
-        public byte? getRoleIdByCurrentAcc(string userId)
+        public int getRoleQuanLy(string userId)
         {
-            return context.Accounts.SingleOrDefault(x => x.User_ID.Equals(userId)).Role_ID;
+            return context.Role_detail.SingleOrDefault(x => x.Role_name.ToLower().Equals("quản lý")).Role_ID;
         }
-        public List<DanhSachNguoiDung> getAllUsersActive()
+        public string getRoleQuanLy()
+        {
+            return context.Role_detail.SingleOrDefault(x => x.Role_name.ToLower().Equals("quản lý")).Role_ID.ToString();
+        }
+        public string getRolePhanPhoi()
+        {
+            return context.Role_detail.SingleOrDefault(x => x.Role_name.ToLower().Equals("phân phối")).Role_ID.ToString();
+        }
+        public string getRoleHangHoa()
+        {
+            return context.Role_detail.SingleOrDefault(x => x.Role_name.ToLower().Equals("hàng hóa")).Role_ID.ToString();
+        }
+        public string getRoleKeToan()
+        {
+            return context.Role_detail.SingleOrDefault(x => x.Role_name.ToLower().Equals("kế toán")).Role_ID.ToString();
+        }
+        public List<DanhSachNguoiDung> getAllUsersActiveByQuanTri()
         {
             List<DanhSachNguoiDung> lst = new List<DanhSachNguoiDung>();
 
             var query = from user in context.Users
                         join media in context.Media on user.Avatar_ID.ToString() equals media.Media_ID.ToString()
-                        //where user
+                        join role in context.Role_detail on user.Role_ID equals role.Role_ID
+                        where user.Status == 1
                         select new
                         {
                             user,
-                            media.Location
+                            media.Location,
+                            role.Role_name
+
                         };
             if (query != null)
             {
-                foreach(var item in query)
+                foreach (var item in query)
                 {
                     DanhSachNguoiDung ds = new DanhSachNguoiDung();
 
 
                     ds.tenNguoiDung = item.user.User_name;
+                    ds.anhDaiDien = item.Location;
+                    ds.ngayTao = Convert.ToDateTime(item.user.Date_created);
+                    ds.phanHe = item.Role_name;
+                    ds.trangThai = item.user.Status == 1 ? "Đang hoạt động" : "";
+
+                    lst.Add(ds);
                 }
             }
+            return lst;
+        }
+        public List<DanhSachNguoiDung> getAllUsersActiveByQuanLy()
+        {
+            string roleQuanly = getRoleQuanLy();
+            string roleHangHoa = getRoleHangHoa();
+            string roleKeToan = getRoleKeToan();
+            string rolePhanPhoi = getRolePhanPhoi();
+
+            List<DanhSachNguoiDung> lst = new List<DanhSachNguoiDung>();
+
+            var query = from user in context.Users
+                        join media in context.Media on user.Avatar_ID.ToString() equals media.Media_ID.ToString()
+                        join role in context.Role_detail on user.Role_ID equals role.Role_ID
+                        where user.Status == 1 && (user.Role_ID.ToString().Equals(roleQuanly)
+                        || user.Role_ID.ToString().Equals(roleHangHoa)
+                        || user.Role_ID.ToString().Equals(roleKeToan)
+                        || user.Role_ID.ToString().Equals(rolePhanPhoi))
+                        select new
+                        {
+                            user,
+                            media.Location,
+                            role.Role_name
+
+                        };
+            if (query != null)
+            {
+                foreach (var item in query)
+                {
+                    DanhSachNguoiDung ds = new DanhSachNguoiDung();
+
+
+                    ds.tenNguoiDung = item.user.User_name;
+                    ds.anhDaiDien = item.Location;
+                    ds.ngayTao = Convert.ToDateTime(item.user.Date_created);
+                    ds.phanHe = item.Role_name;
+                    ds.trangThai = item.user.Status == 1 ? "Đang hoạt động" : "";
+
+                    lst.Add(ds);
+                }
+            }
+
             return lst;
         }
     }
