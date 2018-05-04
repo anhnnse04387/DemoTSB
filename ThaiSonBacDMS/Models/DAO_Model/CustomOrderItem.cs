3c01a5af2ad68b3868991fc384a023631e5af44e
DESKTOP-UNQK7SS\Brdl666 <anhnnse04387@fpt.edu.vn>
2018-05-05 02:33:37 +0700

    update

---
+++ ThaiSonBacDMS/Models/DAO_Model/CustomOrderItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.DAO_Model
+{
+    public class CustomOrderItem
+    {
+        public int id { get; set; }
+        public String orderId { get; set; }
+        public int? qtt { get; set; }
+        public String date { get; set; }
+    }
+}
