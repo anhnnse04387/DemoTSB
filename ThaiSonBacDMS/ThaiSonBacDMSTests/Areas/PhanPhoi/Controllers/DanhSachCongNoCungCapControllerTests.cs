59dd634de8056c2d73da0bd8f115b7895af265a7
Xuan Thuong <thuongtxse03568@fpt.edu.vn>
2018-05-07 14:06:03 +0700

    commit code test

---
+++ ThaiSonBacDMS/ThaiSonBacDMSTests/Areas/PhanPhoi/Controllers/DanhSachCongNoCungCapControllerTests.cs
@@ -0,0 +1,134 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Models.DAO_Model;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using ThaiSonBacDMS.Areas.PhanPhoi.Models;
+
+namespace ThaiSonBacDMS.Areas.PhanPhoi.Controllers.Tests
+{
+    [TestClass()]
+    public class DanhSachCongNoCungCapControllerTests
+    {
+        [TestMethod()]
+        public void IndexTest()
+        {
+            var controller = new DanhSachCongNoCungCapController();
+            var result = controller.Index() as ViewResult;
+            var x = result.Model as DanhSachCongNoCungCapModel;
+            Assert.IsTrue(x.lstDisplay.Count > 0);
+        }
+        //search by all values is null
+        [TestMethod()]
+        public void TestSearchBy_All_NullValues()
+        {
+            var controller = new DanhSachCongNoCungCapController();
+            var result = controller.Index("", "", "") as ViewResult;
+            var x = result.Model as DanhSachCongNoCungCapModel;
+            Assert.IsTrue(x.lstDisplay.Count > 0);
+        }
+        //test insert with all values are null
+        [TestMethod()]
+        public void InsertData_Without_All_Null_Value()
+        {
+            var controller = new DanhSachCongNoCungCapController();
+            var result = controller.InsertData("", "", "", "", "", "") as JsonResult;
+            IDictionary<string, object> data = (IDictionary<string, object>)new System.Web.Routing.RouteValueDictionary(result.Data);
+            Assert.AreEqual(false, data["success"]);
+        }
+        //test insert with supplier id is character
+        [TestMethod()]
+        public void InserData_With_Supplier_Id_Is_Character()
+        {
+            var controller = new DanhSachCongNoCungCapController();
+            var result = controller.InsertData("a", "2,000,000", "500,000", "1,500,000", "0", "") as JsonResult;
+            IDictionary<string, object> data =
+            (IDictionary<string, object>)new System.Web.Routing.RouteValueDictionary(result.Data);
+            Assert.AreEqual(true, data["success"]);
+        }
+        //test insert with supplier id is null
+        [TestMethod()]
+        public void InsertData_Without_Supplier_Id()
+        {
+            var controller = new DanhSachCongNoCungCapController();
+            var result = controller.InsertData("", "2,000,000", "500,000", "1,500,000", "0", "") as JsonResult;
+            IDictionary<string, object> data =
+            (IDictionary<string, object>)new System.Web.Routing.RouteValueDictionary(result.Data);
+            Assert.AreEqual(true, data["success"]);
+        }
+        //test insert with thanhToan is null
+        [TestMethod()]
+        public void InsertData_With_ThanhToan_Is_Null()
+        {
+            var controller = new DanhSachCongNoCungCapController();
+            var result = controller.InsertData("1", "2,000,000", "", "1,500,000", "0", "") as JsonResult;
+            IDictionary<string, object> data =
+            (IDictionary<string, object>)new System.Web.Routing.RouteValueDictionary(result.Data);
+            Assert.AreEqual(true, data["success"]);
+        }
+        //test insert with thanhToan is character
+        [TestMethod()]
+        public void InsertData_With_ThanhToan_Is_Character()
+        {
+            var controller = new DanhSachCongNoCungCapController();
+            var result = controller.InsertData("1", "2,000,000", "a", "1,500,000", "0", "") as JsonResult;
+            IDictionary<string, object> data =
+            (IDictionary<string, object>)new System.Web.Routing.RouteValueDictionary(result.Data);
+            Assert.AreEqual(true, data["success"]);
+        }
+        //test insert with nhapTrongKy is null
+        [TestMethod()]
+        public void InsertData_With_NhapTrongKy_Is_Null()
+        {
+            var controller = new DanhSachCongNoCungCapController();
+            var result = controller.InsertData("1", "", "2,000,000", "1,500,000", "0", "") as JsonResult;
+            IDictionary<string, object> data =
+            (IDictionary<string, object>)new System.Web.Routing.RouteValueDictionary(result.Data);
+            Assert.AreEqual(true, data["success"]);
+        }
+        //test insert with nhapTrongKy is character
+        [TestMethod()]
+        public void InsertData_With_NhapTrongKy_Is_Character()
+        {
+            var controller = new DanhSachCongNoCungCapController();
+            var result = controller.InsertData("1", "a", "2,000,000", "1,500,000", "0", "") as JsonResult;
+            IDictionary<string, object> data =
+            (IDictionary<string, object>)new System.Web.Routing.RouteValueDictionary(result.Data);
+            Assert.AreEqual(true, data["success"]);
+        }
+        //test insert with valid values need session
+        [TestMethod()]
+        public void InsertData_With_Validate_Values()
+        {
+            var controller = new DanhSachCongNoCungCapController();
+            var result = controller.InsertData("1", "4000000", "2000000", "2000000", "5000000", "tra tien hang") as JsonResult;
+            IDictionary<string, object> data =
+            (IDictionary<string, object>)new System.Web.Routing.RouteValueDictionary(result.Data);
+            Assert.AreEqual(true, data["success"]);
+        }
+        //test autocomplete function with null value
+        [TestMethod()]
+        public void autoCompleteTest()
+        {
+            var controller = new DanhSachCongNoCungCapController();
+            var result = controller.autoComplete("") as JsonResult;
+            var x = result.Data;
+            Assert.AreEqual(null, x);
+        }
+        //test autocomplete function with value
+        [TestMethod()]
+        public void Test_Autocomplete_Function_With_Value()
+        {
+            var controller = new DanhSachCongNoCungCapController();
+            var result = controller.autoComplete("LS") as JsonResult;
+            var x = result.Data;
+            Assert.AreEqual(null, x);
+        }
+
+        //test search with
+        [TestMethod()]
+        public void IndexTest1()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ ThaiSonBacDMS/ThaiSonBacDMSTests/Areas/PhanPhoi/Controllers/SanPhamNgungKinhDoanhControllerTests.cs
@@ -0,0 +1,139 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ThaiSonBacDMS.Areas.PhanPhoi.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using ThaiSonBacDMS.Areas.PhanPhoi.Models;
+
+namespace ThaiSonBacDMS.Areas.PhanPhoi.Controllers.Tests
+{
+    [TestClass()]
+    public class SanPhamNgungKinhDoanhControllerTests
+    {
+        [TestMethod()]
+        public void GetSearchValueTest()
+        {
+            var controller = new SanPhamNgungKinhDoanhController();
+            var result = controller.Index() as ViewResult;
+            var x = result.Model as ProductPhanPhoiModel;
+            Assert.IsTrue(x.lstDisplay.Count > 0);
+        }
+
+        //search by all value is null
+        [TestMethod()]
+        public void TestSearchBy_NullValues()
+        {
+            var controller = new SanPhamNgungKinhDoanhController();
+            var model = new ProductPhanPhoiModel();
+            var result = controller.Index(model) as ViewResult;
+            var x = result.Model as ProductPhanPhoiModel;
+            Assert.IsTrue(x.lstDisplay.Count > 0);
+        }
+        //search by product name
+        [TestMethod]
+        public void TestSearchBy_ProductName()
+        {
+            var controller = new SanPhamNgungKinhDoanhController();
+            var model = new ProductPhanPhoiModel();
+            model.productIdSearch = "3";
+            var result = controller.Index(model) as ViewResult;
+            var x = result.Model as ProductPhanPhoiModel;
+            Assert.IsTrue(x.lstDisplay.Count > 0);
+
+        }
+        //search by supplier
+        [TestMethod()]
+        public void TestSearchBy_Supplier()
+        {
+            var controller = new SanPhamNgungKinhDoanhController();
+            var model = new ProductPhanPhoiModel();
+            model.supplierSearch = "9";
+            var result = controller.Index(model) as ViewResult;
+            var x = result.Model as ProductPhanPhoiModel;
+            Assert.IsTrue(x.lstDisplay.Count > 0);
+        }
+        //search by date import from
+        [TestMethod()]
+        public void TestSearchBy_DateImport_From()
+        {
+            var controller = new SanPhamNgungKinhDoanhController();
+            var model = new ProductPhanPhoiModel();
+            model.fromDate = "03/10/2018";
+            var result = controller.Index(model) as ViewResult;
+            var x = result.Model as ProductPhanPhoiModel;
+            Assert.IsTrue(x.lstDisplay.Count > 0);
+        }
+        //search by date import to
+        [TestMethod()]
+        public void TestSearchBy_DateImport_To()
+        {
+            var controller = new SanPhamNgungKinhDoanhController();
+            var model = new ProductPhanPhoiModel();
+            model.toDate = "03/10/2018";
+            var result = controller.Index(model) as ViewResult;
+            var x = result.Model as ProductPhanPhoiModel;
+            Assert.IsTrue(x.lstDisplay.Count > 0);
+        }
+        //search by date import from and to
+        [TestMethod()]
+        public void TestSearchBy_DateImport_From_To()
+        {
+            var controller = new SanPhamNgungKinhDoanhController();
+            var model = new ProductPhanPhoiModel();
+            model.fromDate = "03/10/2018";
+            model.toDate = "05/25/2018";
+            var result = controller.Index(model) as ViewResult;
+            var x = result.Model as ProductPhanPhoiModel;
+            Assert.IsTrue(x.lstDisplay.Count > 0);
+        }
+        //search by category
+        [TestMethod()]
+        public void TestSearchBy_Category()
+        {
+            var controller = new SanPhamNgungKinhDoanhController();
+            var model = new ProductPhanPhoiModel();
+            model.categorySearch = "10";
+            var result = controller.Index(model) as ViewResult;
+            var x = result.Model as ProductPhanPhoiModel;
+            Assert.IsTrue(x.lstDisplay.Count > 0);
+        }
+        //search all values
+        [TestMethod()]
+        public void TestSearchBy_AllValue()
+        {
+            var controller = new SanPhamNgungKinhDoanhController();
+            var model = new ProductPhanPhoiModel();
+            model.productIdSearch = "2";
+            model.categorySearch = "1";
+            model.supplierSearch = "1";
+            model.fromDate = "03/10/2018";
+            model.toDate = "05/25/2018";
+            var result = controller.Index(model) as ViewResult;
+            var x = result.Model as ProductPhanPhoiModel;
+            Assert.IsTrue(x.lstDisplay.Count > 0);
+        }
+        //test search value is null
+        [TestMethod()]
+        public void TestSearchNameIsNull()
+        {
+            var controller = new SanPhamNgungKinhDoanhController();
+            var result = controller.GetSearchValue("") as JsonResult;
+            var x = result.Data;
+            Assert.IsTrue(x !=null);
+            
+        }
+        //test search value is not null
+        [TestMethod()]
+        public void TestSearchNameIsNotNull()
+        {
+            var controller = new SanPhamNgungKinhDoanhController();
+            var result = controller.GetSearchValue("MC2") as JsonResult;
+            var x = result.Data;
+            Assert.IsTrue(x != null);
+
+        }
+    }
+}
