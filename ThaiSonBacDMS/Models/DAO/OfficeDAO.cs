433ecf1d5de8a07fc842894bf05d30adc29a2e46
Xuan Thuong <thuongtxse03568@fpt.edu.vn>
2018-05-10 14:38:00 +0700

    commit code

--- ThaiSonBacDMS/Models/DAO/OfficeDAO.cs
+++ ThaiSonBacDMS/Models/DAO/OfficeDAO.cs
@@ -30,7 +30,7 @@ namespace Models.DAO
                         };
             if (query.Count() != 0)
             {
-                foreach(var item in query)
+                foreach (var item in query)
                 {
                     Autocomplete au = new Autocomplete();
                     au.key = item.office.Office_name;
@@ -40,6 +40,48 @@ namespace Models.DAO
             }
             return lst;
         }
+
+        public List<Autocomplete> getListCreatUser(string selectedValue)
+        {
+
+
+            List<Autocomplete> lst = new List<Autocomplete>();
+            if (!selectedValue.Contains(","))
+            {
+                var query = from office in db.Offices
+                            where office.Role_ID.ToString().Equals(selectedValue)
+                            select office;
+                if (query.Count() != 0)
+                {
+                    foreach (var item in query)
+                    {
+                        Autocomplete au = new Autocomplete();
+                        au.key = item.Office_name;
+                        au.value = item.Office_ID;
+                        lst.Add(au);
+                    }
+                }
+            }
+            else
+            {
+                string[] values = selectedValue.Split(',');
+                var query = from office in db.Offices
+                            where values.Contains(office.Role_ID.ToString())
+                            select office;
+                if (query.Count() != 0)
+                {
+                    foreach (var item in query)
+                    {
+                        Autocomplete au = new Autocomplete();
+                        au.key = item.Office_name;
+                        au.value = item.Office_ID;
+                        lst.Add(au);
+                    }
+                }
+            }
+
+            return lst;
+        }
     }
-   
+
 }
