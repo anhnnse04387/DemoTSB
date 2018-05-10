11e81a54d7309d8d106ed2fd40c322f0ec688992
DESKTOP-UNQK7SS\Brdl666 <anhnnse04387@fpt.edu.vn>
2018-05-10 14:35:47 +0700

    update

--- ThaiSonBacDMS/Models/DAO/OrderPartDAO.cs
+++ ThaiSonBacDMS/Models/DAO/OrderPartDAO.cs
@@ -26,21 +26,38 @@ namespace Models.DAO
             }
             return null;
         }
-        
+
+        public void updateOrderPart(Order_part part, bool takeInvoice, bool takeBallot)
+        {
+            var p = db.Order_part.Where(x => x.Order_part_ID.Equals(part.Order_part_ID)).SingleOrDefault();
+            p.Shiper_ID = part.Shiper_ID;
+            p.Driver_ID = part.Driver_ID;
+            p.DeliverMethod_ID = part.DeliverMethod_ID;
+            if (takeInvoice)
+            {
+                p.Date_take_invoice = DateTime.Now;
+            }
+            if (takeBallot)
+            {
+                p.Date_take_ballot = DateTime.Now;
+            }
+            db.SaveChanges();
+        }
+
         public List<Order_part> getByCreatedDate(DateTime beginDate, DateTime endDate)
         {
             return db.Order_part.Where(x => x.Date_created >= beginDate && x.Date_created <= endDate).ToList();
         }
-        
+
         public Order_part getByName(string orderPartName)
         {
             return db.Order_part.Where(x => x.Order_part_ID == orderPartName).SingleOrDefault();
         }
-        
+
         public List<Order_part> getAllOrderPartByStatus(int status_ID)
         {
             return db.Order_part.Where(x => x.Status_ID == status_ID).ToList();
-        }        
+        }
 
         public List<Order_part> getAllOrderPart()
         {
